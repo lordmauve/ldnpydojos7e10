676a338008885dc858791ceff1dd54713a1f8be6
Ski <skyjur@gmail.com>
2016-06-09 20:47:19 +0100

    update

--- wikigame.py
+++ wikigame.py
@@ -1,3 +1,25 @@
+def load_graph():
+    g = {}
+
+    lines = list(open('wikipedia.txt'))
+
+    for ln in lines:
+        node = int(ln.split(':')[0])
+        g[node] = []
+
+    for i, ln in enumerate(lines):
+        node = int(ln.split(':')[0])
+        links = ln.split(':')[1]
+        for link in links.strip().split():
+            g[node].append(int(link))
+
+        if i % 1000 == 0:
+            print i / float(len(lines))
+
+    return g
+
+
+
 def try_find_path(g, start, finish):
     paths = [[start]] 
 
